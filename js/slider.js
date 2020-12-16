adaf1f1a677cbcc04fa38a66f905e93e33f9f1cb
Bartosz Czechowicz <bartosz7czechowicz@gmail.com>
2020-12-16 14:03:00 +0100

    recruitment task

---
+++ js/slider.js
@@ -0,0 +1,16 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // Pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
